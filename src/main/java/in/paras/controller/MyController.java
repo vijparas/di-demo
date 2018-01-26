b228d17e2ba06f804418496e8e3d1deb2589874d
Paras <vijparas@gmail.com>
2018-01-26 19:40:52 +0530

    In this we are using Primary Annonation to make a primary bean so if no qualifier is defined the primary bean is picked up as default bean.

--- src/main/java/in/paras/controller/MyController.java
+++ src/main/java/in/paras/controller/MyController.java
@@ -1,11 +1,20 @@
 package in.paras.controller;
 
+import in.paras.services.GreetingService;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 
 @Controller
+
 public class MyController {
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello(){
-        System.out.println("Hello world");
-        return "foo";
+
+        return greetingService.sayGreeting();
     }
 }
--- src/main/java/in/paras/controller/PropertyInjectedController.java
+++ src/main/java/in/paras/controller/PropertyInjectedController.java
@@ -1,14 +1,17 @@
 package in.paras.controller;
 
+import in.paras.services.GreetingService;
 import in.paras.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingServiceImpl greetingService;
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingService;
 
     public String sayHello(){
         return greetingService.sayGreeting();
